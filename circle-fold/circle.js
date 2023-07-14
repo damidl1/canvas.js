96a10a11b07ed4f39d52268f06209d0d2fdb9d02
Doxstart <135213161+damidl1@users.noreply.github.com>
2023-07-14 13:14:45 +0200

    inizio task

---
+++ circle-fold/circle.js
@@ -0,0 +1,8 @@
+class Circle{
+
+    constructor(originX, originY, radius){
+        this.originX = originX;
+        this.originY = originY;
+        this.radius = radius;
+    }
+}
---
+++ circle-fold/main-circle.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('canvas1');
+
+const ctx = canvas.getContext('2d');
+
+ctx.beginPath();
+ctx.arc(100, 75, 50, 0, 2* Math.PI );
+ctx.fillStyle = "blue";
+ctx.fillStyle()
+
+
